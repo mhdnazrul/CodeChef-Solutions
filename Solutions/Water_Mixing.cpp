f48ba8ed540bca5b2cd0366f7a7f81ef8003c1f4
Nazrul Islam <mhdnazrul511@gmail.com>
2025-12-13 14:41:13 +0600

    Add new solutions

---
+++ Solutions/Water_Mixing.cpp
@@ -0,0 +1,44 @@
+/*┌────────────────────────────────────────────────────────────────────────────────────┐
+  │                        >   Handle:- nazrulislam_7             
+  │                        >   Author:- Nazrul Islam                   
+  │Problem Link: https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/WTRMIXING
+  └────────────────────────────────────────────────────────────────────────────────────┘*/
+#include <bits/stdc++.h>
+using namespace std;
+using ll=long long;      using ld=long double;
+using vi=vector<int>;    using vll=vector<ll>;
+using pii=pair<int,int>; using pll=pair<ll,ll>;
+constexpr ll INF=4e18;   constexpr int MOD=1e9+7;
+const char nl='\n';
+#define sz(x) (int)(x.size())
+#define all(x) begin(x),end(x)
+#define rall(x) rbegin(x),rend(x)
+#define fastio() ios::sync_with_stdio(false); cin.tie(nullptr)
+template<class T> void in(T &x){cin>>x;}
+template<class T> void inV(vector<T> &v){for(auto &x:v) cin>>x;}
+template<class T> void out(const T &x){cout<<x<<'\n';}
+template<class T> void outV(const vector<T> &v){for(int i=0;i<(int)v.size();i++) cout<<v[i]<<(i+1<(int)v.size()?' ':'\n');}
+
+void run_case(){
+    int a,b,x,y; cin>>a>>b>>x>>y;
+    if(a==b){out("YES");return;}
+    if(a<b){
+      int c = b - a;
+      if(c<=x) out("YES");
+      else out("NO");
+      return;
+    }
+    if(b<a){
+      int c = a - b;
+      if(c<=y) out("YES");
+      else out("NO");
+      return;
+    }
+}
+
+int32_t main(){
+    fastio();      int T=1; 
+    if(!(cin>>T))  return 0;
+    while(T--)     run_case();
+    return 0;
+}
