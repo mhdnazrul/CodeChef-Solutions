15736c9df75880ea0d8bb0b70a3b37d37affb2a7
Nazrul Islam <mhdnazrul511@gmail.com>
2026-01-14 20:56:11 +0600

    Add new solutions

---
+++ Solutions/Winter_is_Coming.cpp
@@ -0,0 +1,45 @@
+/*┌────────────────────────────────────────────────────────────────────────────────────┐
+  │                        >   Handle:- nazrulislam_7             
+  │                        >   Author:- Nazrul Islam                   
+  │Problem Link: https://www.codechef.com/START221C/problems/LMP2E
+  └────────────────────────────────────────────────────────────────────────────────────┘*/
+#include <bits/stdc++.h>
+using namespace std;
+using ll=long long;      using ld=long double;
+using vi=vector<int>;    using vll=vector<ll>;
+using pii=pair<int,int>; using pll=pair<ll,ll>;
+constexpr ll INF=4e18;   constexpr int MOD=1e9+7;
+const char nl='\n';
+#define sz(x) (int)(x.size())
+#define all(x) begin(x),end(x)
+#define rall(x) rbegin(x),rend(x)
+#define fastio() ios::sync_with_stdio(false); cin.tie(nullptr)
+template<class T> void in(T &x){cin>>x;}
+template<class T> void inV(vector<T> &v){for(auto &x:v) cin>>x;}
+template<class T> void out(const T &x){cout<<x<<'\n';}
+template<class T> void outV(const vector<T> &v){for(int i=0;i<(int)v.size();i++) cout<<v[i]<<(i+1<(int)v.size()?' ':'\n');}
+
+void run_case(){
+    int n,a,b;
+    cin>>n>>a>>b;
+    bool ok=0;
+    int cnt=0;
+    for(int i=0; i<n; i++){
+        int x;  cin>>x;
+        if(x<a){
+            if(!ok){
+                cnt++;
+                ok=1;
+            }
+        }
+        else if(x>b)ok=0;
+    }
+    out(cnt);
+}
+
+int32_t main(){
+    fastio();      int T=1; 
+    if(!(cin>>T))  return 0;
+    while(T--)     run_case();
+    return 0;
+}
