1812224c2da7f0e1b90e4a6bb593491a14113651
Nazrul Islam <mhdnazrul511@gmail.com>
2026-01-15 08:48:00 +0600

    Add new solutions

--- Solutions/Find_Remainder.cpp
+++ Solutions/Find_Remainder.cpp
@@ -1,7 +1,7 @@
 /*┌────────────────────────────────────────────────────────────────────────────────────┐
   │                        >   Handle:- nazrulislam_7             
   │                        >   Author:- Nazrul Islam                   
-  │Problem Link: https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/FSQRT
+  │Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/FLOW002
   └────────────────────────────────────────────────────────────────────────────────────┘*/
 #include <bits/stdc++.h>
 using namespace std;
@@ -20,9 +20,9 @@ template<class T> void out(const T &x){cout<<x<<'\n';}
 template<class T> void outV(const vector<T> &v){for(int i=0;i<(int)v.size();i++) cout<<v[i]<<(i+1<(int)v.size()?' ':'\n');}
 
 void run_case(){
-    int n; cin>>n;
-    int res= sqrt(n);
-    cout<< res<<nl;
+    int a, b;
+    cin >> a >> b;
+    cout << a % b << endl;
 }
 
 int32_t main(){
