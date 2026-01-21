f9d57f905b6ed51881dca45309e91ed3cc24c0f5
Nazrul Islam <mhdnazrul511@gmail.com>
2026-01-21 21:54:46 +0600

    Add new solutions

---
+++ Solutions/Layered_Cake.cpp
@@ -0,0 +1,43 @@
+/*┌────────────────────────────────────────────────────────────────────────────────────┐
+  │                        >   Handle:- nazrulislam_7             
+  │                        >   Author:- Nazrul Islam                   
+  │Problem Link: https://www.codechef.com/START222D/problems/LAYERCAKE
+  └────────────────────────────────────────────────────────────────────────────────────┘*/
+#include <bits/stdc++.h>
+using namespace std;
+using ll=long long;       using ld=long double;
+using vi=vector<int>;     using vll=vector<ll>;
+using pii=pair<int,int>; using pll=pair<ll,ll>;
+const ll INF = 2e18;   const int MOD = 1e9 + 7; 
+#define per(i,a,b)  for(int i=(b)-1;i>=(a);--i)
+#define rep(i,a,b)  for(int i=(a);i<(b);++i)
+#define trav(a, x)     for (auto &a : x)
+#define forr(i, n)       rep(i, 0, n)
+#define all(x) (x).begin(),(x).end()
+#define rall(x) rbegin(x),rend(x)
+#define sz(x) (int)(x).size()
+const char nl = '\n';
+template<class T>    void inV(vector<T> &v){for(auto &x:v) cin>>x;}
+template<typename T> void outV(const vector<T>& v) {if (v.empty()) return;
+    for (int i = 0; i < sz(v); ++i)cout << v[i] << " \n"[i == sz(v) - 1];}
+
+void run_case(){
+    int n,m;      cin >> n >> m;
+    vi a(n), b(m);   inV(a);   inV(b);
+    int ans =0;
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++){
+            if(a[i]>b[j])ans++;
+        }
+    }
+    cout << ans << nl;
+}
+
+int32_t main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);          int T=1;
+    if(!(cin >> T))     return 0;
+    while(T--)        run_case();
+    return 0;
+}
+
